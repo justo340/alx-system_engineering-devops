87fa0768f11a164128c4b517020889625addffe5
justo340 <justusnjogu16@gmail.com>
2023-03-25 02:55:53 -0700

    using puppet

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,5 @@
+# Install a package
+package { 'puppet-lint':
+  ensure   => '2.1.0',
+  provider => 'flask',
+}
---
+++ 0x0A-configuration_management/README.md
@@ -0,0 +1,1 @@
+0x0A Configuration management Foundations - System engineering & DevOps ― CI/CD
