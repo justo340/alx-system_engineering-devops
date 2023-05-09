aa8eb8635180e1772ee56565fe54ece1c8a08988
justo340 <justusnjogu16@gmail.com>
2023-05-09 13:34:29 -0700

    advanced api learning

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""Module with top_ten function"""
+import re
+import requests
+import sys
+
+
+def count_words(subreddit, word_list, after="", to_print={}):
+    """Queries the Reddit API and returns a list containing the titles of all
+    hot articles for a given subreddit."""
+
+    if after == "":
+        for word in word_list:
+            to_print[word] = 0
+
+    url = "https://www.reddit.com/r/{}/hot.json{}".format(subreddit, after)
+    json_obj = requests.get(url, headers={'User-Agent': 'MyUserAgent'})
+
+    if json_obj.status_code != 404:
+
+        dict_obj = json_obj.json()
+        list_obj = dict_obj.get('data').get('children')
+
+        for each in list_obj:
+            title = each.get('data').get('title')
+            tit_list = title.split()
+            for word in word_list:
+                c = re.compile(r"^{}$".format(word), re.I)
+                for each_tit_w in tit_list:
+                    res = c.findall(each_tit_w)
+                    to_print[word] += len(res)
+
+        next_fullname = dict_obj.get('data').get('after')
+        after = "?after={}".format(next_fullname)
+
+        if next_fullname is not None:
+            count_words(subreddit, word_list, after, to_print)
+        else:
+            sorted_l = sorted(to_print.items(), key=lambda x: x[1])
+            sorted_l.reverse()
+
+            for x in sorted_l:
+                if x[1] != 0:
+                    print("{}: {}".format(x[0], x[1]))
+
+    return None
